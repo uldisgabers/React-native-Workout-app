bf31eea7f339e4b2c0f0215a422c5d2ccd8b6f2b
Uldis Gabers <uldisgabers@gmail.com>
2024-02-09 23:41:01 +0200

    init commit

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "play on the switch", key: "3" },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* header */}
+      <View style={styles.content}>
+        {/* to do form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +39,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+
   },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  }
 });
---
+++ components/header.js
@@ -0,0 +1,10 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
