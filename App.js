2a612dfad88e414699c498ce4c1b417be61e9b5c
Uldis Gabers <uldisgabers@gmail.com>
2024-02-10 01:16:13 +0200

    updates to components

--- App.js
+++ App.js
@@ -6,8 +6,14 @@ import {
   ScrollView,
   FlatList,
   TouchableOpacity,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
 import { useState } from "react";
+import Header from "./components/header";
+import TodoItem from "./components/todoItem";
+import AddTodo from "./components/addTodo";
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -16,23 +22,46 @@ export default function App() {
     { text: "play on the switch", key: "3" },
   ]);
 
-  return (
-    <View style={styles.container}>
-      {/* header */}
-      <View style={styles.content}>
-        {/* to do form */}
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem={({ item }) => (
-              <Text>{item.text}</Text>
-            )}
-          />
-        </View>
-      </View>
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key !== key);
+    });
+  };
 
-      <StatusBar style="auto" />
-    </View>
+  const submitHandler = (text) => {
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+      });
+    } else {
+      Alert.alert("OOPS!!", "Todos must be over 3 characters long", [
+        { text: "Understood", onPress: () => console.log("alert closed") },
+      ]);
+    }
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss()
+      console.log('dismissed keyboard')
+    }}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+        </View>
+
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -40,12 +69,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-
   },
   content: {
     padding: 40,
   },
   list: {
     marginTop: 20,
-  }
+  },
 });
--- components/header.js
+++ components/header.js
@@ -8,3 +8,25 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { useState } from "react";
+
+export default function Header() {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>MY TODOS</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    height: 80,
+    paddingTop: 38,
+    backgroundColor: "coral"
+  },
+  title: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+})
