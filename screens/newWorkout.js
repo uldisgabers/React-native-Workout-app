1161e71348c0b0068550ddb9edd0877ea9d9c737
Uldis Gabers <uldisgabers@gmail.com>
2024-02-20 18:06:07 +0200

    workout list

--- screens/newWorkout.js
+++ screens/newWorkout.js
@@ -6,31 +6,34 @@ import {
   Button,
   TouchableOpacity,
   Image,
+  FlatList,
 } from "react-native";
+const { v4: uuidv4 } = require("uuid");
 import React, { useState, useEffect } from "react";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { getExerciseDataByMuscle } from "../utils/fetchData";
 import DropdownMuscleGroup from "../components/dropdownMuscleGroup";
-import { exerciseData } from "../utils/dummyData";
 import DropdownExerciseName from "../components/dropdownExerciseName";
 import SetsSelector from "../components/setsSelector";
 import SetsLengthSelector from "../components/setsLengthSelector";
 import SetsRestSelector from "../components/setsRestSlector";
 
+// const workoutTree = [];
+
 const NewWorkoutScreen = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
-
-  // getExerciseDataByMuscle(muscle).then((result) => {
-  //   // console.log(result[0].name);
-  // });
+  const [workoutTree, setWorkoutTree] = useState([]);
 
   const [newExercise, setNewExercise] = useState({
+    id: uuidv4(),
     muscleGroup: "",
     name: "",
     sets: 1,
-    oneSetLength: 40,
-    restLength: 20,
+    oneSetLength: 40, // default seconds for set length
+    restLength: 20, // default seconds for rest length
   });
+
+  // const [workoutTree, setWorkoutTree] = useState([]);
 
   // add muscle group to the data object
 
@@ -84,10 +87,38 @@ const NewWorkoutScreen = () => {
     setNewExercise({ ...newExercise, restLength: value });
   };
 
+  // add exercise to the workout tree
+
+  const addExerciseToWorkoutTree = (exercise) => {
+    setWorkoutTree([...workoutTree, exercise]);
+    setNewExercise({
+      id: uuidv4(),
+      muscleGroup: "",
+      name: "",
+      sets: 1,
+      oneSetLength: 40,
+      restLength: 20,
+    });
+  };
+
+  console.log(workoutTree);
+
+  // POST workout to db
+
+  const saveWorkoutToDB = () => {
+
+  }
+
   return (
-    <View>
-      <Text>New Workout Screen</Text>
-      <Button title="Add exercise" onPress={() => setIsModalVisible(true)} />
+    <View style={{ flex: 1, alignItems: "center" }}>
+      {/* <Text>New Workout Screen</Text> */}
+      <TouchableOpacity
+        style={styles.addExerciseBtn}
+        onPress={() => setIsModalVisible(true)}
+      >
+        <Text style={styles.buttonText}>ADD EXERCISE</Text>
+      </TouchableOpacity>
+      {/* <Button title="Add exercise" onPress={() => setIsModalVisible(true)} /> */}
       <Modal
         visible={isModalVisible}
         animationType="slide"
@@ -116,7 +147,12 @@ const NewWorkoutScreen = () => {
               <SetsLengthSelector addSetLength={addSetLength} />
               <SetsRestSelector addSetRestLength={addSetRestLength} />
 
-              <TouchableOpacity onPress={() => setIsModalVisible(false)}>
+              <TouchableOpacity
+                onPress={() => {
+                  addExerciseToWorkoutTree(newExercise);
+                  setIsModalVisible(false);
+                }}
+              >
                 <View style={styles.addButton}>
                   <Text style={styles.buttonText}>ADD EXERCISE</Text>
                 </View>
@@ -125,7 +161,49 @@ const NewWorkoutScreen = () => {
           )}
         </View>
       </Modal>
-      <Text>{JSON.stringify(newExercise)}</Text>
+
+      {/* <Text>{JSON.stringify(newExercise)}</Text> */}
+
+      <View style={styles.flatlist}>
+        <FlatList
+          data={workoutTree}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={styles.exerciseBlock}>
+              <MaterialCommunityIcons
+                name="square-edit-outline"
+                size={24}
+                color="white"
+              />
+              <View style={{ flex: 1, alignItems: "center" }}>
+                <Text style={styles.buttonText}>{item.name}</Text>
+                <Text style={styles.buttonText}>Sets: {item.sets}</Text>
+              </View>
+              <MaterialCommunityIcons
+                name="delete"
+                size={24}
+                color="white"
+                onPress={() => {
+                  setWorkoutTree(
+                    workoutTree.filter((exercise) => item.id !== exercise.id)
+                  );
+                }}
+              />
+            </View>
+          )}
+        />
+      </View>
+
+      {/* {workoutTree.length !== 0 && (
+        <View style={styles.exerciseBlock}>
+          <Text>{workoutTree[0].name}</Text>
+          <Text>Sets: {workoutTree[0].sets}</Text>
+        </View>
+      )} */}
+
+      <TouchableOpacity style={styles.saveBtn} onPress={saveWorkoutToDB}>
+        <Text style={{ color: "white", fontWeight: "bold" }}>SAVE</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -136,6 +214,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#ddd",
     padding: 20,
     alignItems: "center",
+  },
+  addExerciseBtn: {
+    maxWidth: 150,
+    paddingHorizontal: 20,
+    backgroundColor: "#FF2C07",
+    borderRadius: 20,
+    paddingVertical: 10,
+    alignItems: "center",
+    marginVertical: 5,
   },
   closeButton: {
     width: 32,
@@ -153,6 +240,30 @@ const styles = StyleSheet.create({
   buttonText: {
     color: "white",
     fontWeight: "bold",
+  },
+  exerciseBlock: {
+    flex: 1,
+    backgroundColor: "tomato",
+    padding: 8,
+    marginHorizontal: 10,
+    marginVertical: 5,
+    borderRadius: 10,
+    alignItems: "center",
+    color: "white",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 30,
+  },
+  flatlist: {
+    flex: 1,
+    width: "100%",
+  },
+  saveBtn: {
+    backgroundColor: "#ffa807",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 20,
+    marginVertical: 5,
   },
 });
 
