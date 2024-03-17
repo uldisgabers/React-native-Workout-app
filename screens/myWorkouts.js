fff4e3933cc135b4a05fdfc7650d246675419e5e
Uldis Gabers <uldisgabers@gmail.com>
2024-03-18 01:54:05 +0200

    myWorkouts section added a stack nav

--- screens/myWorkouts.js
+++ screens/myWorkouts.js
@@ -1,18 +1,46 @@
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useFocusEffect } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import WorkoutPlayer from "./workoutPlayer";
+
+const Stack = createNativeStackNavigator();
 
 const MyWorkoutsScreen = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="AllWorkouts"
+        component={AllWorkouts}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="WorkoutPlayer"
+        component={WorkoutPlayer}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const AllWorkouts = ({ navigation }) => {
   const [isLoading, setLoading] = useState(true);
   const [workoutData, setWorkoutData] = useState([]);
 
   const getWorkoutData = async () => {
     try {
-      const response = await fetch('http://172.27.208.1:3001/workouts', {
+      const response = await fetch("http://172.27.208.1:3001/workouts", {
         headers: {
-          'Content-Type': 'application/json'
-        }
+          "Content-Type": "application/json",
+        },
       });
       const json = await response.json();
       setWorkoutData(json.workouts);
@@ -29,7 +57,7 @@ const MyWorkoutsScreen = () => {
 
   useFocusEffect(() => {
     getWorkoutData();
-  })
+  });
 
   return (
     <View style={{ flex: 1 }}>
@@ -38,9 +66,14 @@ const MyWorkoutsScreen = () => {
         numColumns={2}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <View style={styles.itemContainer}>
+          <TouchableOpacity
+            style={styles.itemContainer}
+            onPress={() => {
+              navigation.navigate("WorkoutPlayer");
+            }}
+          >
             <Text>{item.workoutName}</Text>
-          </View>
+          </TouchableOpacity>
         )}
       />
     </View>
@@ -49,7 +82,7 @@ const MyWorkoutsScreen = () => {
 
 const styles = StyleSheet.create({
   itemContainer: {
-    flex: 1/2,
+    flex: 1 / 2,
     backgroundColor: "#64ad6a",
     margin: 5,
     padding: 15,
---
+++ screens/workoutPlayer.js
@@ -0,0 +1,23 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const WorkoutPlayer = () => {
+  return (
+    <View>
+      <Text>WorkoutPlayer</Text>
+      <Text>WorkoutPlayer</Text>
+      <Text>WorkoutPlayer</Text>
+      <Text>WorkoutPlayer</Text>
+      <Text>WorkoutPlayer</Text>
+      <Text>WorkoutPlayer</Text>
+      <Text>WorkoutPlayer</Text>
+      <Text>WorkoutPlayer</Text>
+      <Text>WorkoutPlayer</Text>
+      <Text>WorkoutPlayer</Text>
+      <Text>WorkoutPlayer</Text>
+      <Text>WorkoutPlayer</Text>
+    </View>
+  )
+}
+
+export default WorkoutPlayer
